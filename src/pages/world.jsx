3dcf4598360e9216bf726969537f9fcc75bd09f0
Manolingam N <manolingam1996@gmail.com>
2021-09-16 13:42:36 +0530

    improved ux of map minter with infinite scroll

--- src/pages/world.jsx
+++ src/pages/world.jsx
@@ -1,50 +1,24 @@
 import { useState, useEffect, useContext } from 'react';
 import {
-  SimpleGrid,
   Flex,
-  Box,
   Heading,
   Text,
   Button,
   Link,
-  Tabs,
-  Tab,
-  TabList,
-  TabPanels,
-  TabPanel,
   Modal,
   ModalOverlay,
   ModalContent,
-  ModalCloseButton
+  ModalCloseButton,
+  Spinner
 } from '@chakra-ui/react';
 import { useQuery } from '@apollo/client';
-import { useHistory } from 'react-router-dom';
-import styled from '@emotion/styled';
 
 import { AppContext } from '../context/AppContext';
 
 import { GET_ALL_MAPS } from '../graphql/queries';
-import { theme } from '../theme/index';
 import { mintMap } from '../utils/web3';
 
-const StyledInput = styled.input`
-  width: 200px;
-  height: 50px;
-  outline: none;
-  color: white;
-  font-family: ${theme.fonts.spaceMono};
-  font-size: 1rem;
-  border-radius: 3px;
-  background-color: ${theme.colors.blackLighter};
-  margin-right: 1rem;
-  padding: 10px;
-  &::placeholder {
-    color: #ff3864;
-    opacity: 1;
-  }
-`;
-
-let grids = [];
+import MapTiles from '../components/MapTiles';
 
 const World = () => {
   const context = useContext(AppContext);
@@ -55,36 +29,33 @@ const World = () => {
     },
     notifyOnNetworkStatusChange: true
   });
-  const history = useHistory();
 
-  const [canvasDrawn, setCanvasDrawn] = useState(false);
-  const [tokenIdInput, setTokenIdInput] = useState(10000);
+  const [grids, setGrids] = useState([]);
 
-  const [loading, setLoading] = useState(false);
   const [txInitiated, setTxInitiated] = useState(false);
   const [txHash, setTxhash] = useState('');
   const [modal, setModal] = useState(false);
+  const [mintTokenId, setMintTokenId] = useState('');
 
   if (error) console.log(`Error! ${error.message}`);
 
-  const connect = async () => {
+  const connect = async (_tokenId) => {
     try {
-      setLoading(true);
       if (context.ethersProvider === '') {
         await context.connectWallet();
       }
+      setMintTokenId(_tokenId);
       setModal(true);
-      setLoading(false);
       setModal(true);
     } catch (err) {
-      setLoading(false);
+      console.log(err);
     }
   };
 
   const mint = async () => {
     try {
       setTxInitiated(true);
-      let tx = await mintMap(parseInt(tokenIdInput), context.ethersProvider);
+      let tx = await mintMap(parseInt(mintTokenId), context.ethersProvider);
       if (tx) {
         setTxhash(tx.hash);
         const { status } = await tx.wait();
@@ -101,17 +72,7 @@ const World = () => {
   };
 
   useEffect(() => {
-    grids = [];
-  }, []);
-
-  useEffect(() => {
     if (data) {
-      let _temp = grids;
-
-      data.maps.forEach((map) => {
-        _temp.push(parseInt(map.tokenId));
-      });
-
       if (data.maps.length === 1000) {
         fetchMore({
           variables: {
@@ -126,153 +87,130 @@ const World = () => {
           }
         });
       } else {
-        grids.sort(function (a, b) {
+        let claimedMapIds = [];
+        let tempGrids = [];
+        data.maps.forEach((map) => {
+          claimedMapIds.push(parseInt(map.tokenId));
+        });
+        for (let i = 1; i <= 10000; i++) {
+          if (claimedMapIds.includes(i)) {
+            tempGrids.push({ id: i, claimed: true });
+          } else {
+            tempGrids.push({ id: i, claimed: false });
+          }
+        }
+
+        tempGrids.sort(function (a, b) {
           return a - b;
         });
-        drawGrid(grids);
-        setCanvasDrawn(true);
+
+        setGrids(tempGrids);
       }
     }
   }, [data]);
 
-  const drawGrid = (_grids) => {
-    let w = 4000;
-    let h = 4000;
-    let step = 40;
-    var canvas = document.getElementById('grid');
+  // const drawGrid = (_grids) => {
+  //   let w = 4000;
+  //   let h = 4000;
+  //   let step = 40;
+  //   var canvas = document.getElementById('grid');
 
-    canvas.width = 4000;
-    canvas.height = 4000;
+  //   canvas.width = 4000;
+  //   canvas.height = 4000;
 
-    var ctx = canvas.getContext('2d');
+  //   var ctx = canvas.getContext('2d');
 
-    var count = 1;
-    ctx.beginPath();
+  //   canvas.addEventListener('click', function (event) {
+  //     const rect = canvas.getBoundingClientRect();
 
-    for (var x = 0; x <= w; x += step) {
-      ctx.moveTo(x, 0);
-      ctx.lineTo(x, h);
-    }
-    // set the color of the line
-    ctx.strokeStyle = theme.colors.red;
-    ctx.lineWidth = 1;
+  //     var x = event.clientX - rect.left,
+  //       y = event.clientY - rect.top;
 
-    // the stroke will actually paint the current path
-    ctx.stroke();
+  //     console.log(Math.ceil(y / 10), Math.ceil(x / 10));
 
-    // for the sake of the example 2nd path
-    ctx.beginPath();
-    for (var y = 0; y <= h; y += step) {
-      ctx.moveTo(0, y);
-      ctx.lineTo(w, y);
-    }
-    // set the color of the line
-    ctx.strokeStyle = 'rgb(20,20,20)';
-    // just for fun
-    ctx.lineWidth = 1;
-    // for your original question - you need to stroke only once
-    ctx.stroke();
+  //     let tile = Math.ceil(x / 10) + Math.ceil(y / 10) * 117 - 117;
+  //     console.log(tile);
 
-    for (var y1 = 40; y1 <= h; y1 += step) {
-      for (var x1 = 0; x1 <= w - 40; x1 += step) {
-        ctx.fillText('' + count, x1, y1);
+  //     // if (_grids.indexOf(tile) !== -1) {
+  //     //   window.open(`https://www.monstermaps.world/map/${tile}`, '_blank');
+  //     // }
+  //   });
 
-        if (_grids.includes(count)) {
-          ctx.fillStyle = theme.colors.red;
-          ctx.globalAlpha = 0.5;
-          ctx.fillRect(x1, y1 - step, step, step);
-          ctx.globalAlpha = 1;
-          ctx.fillStyle = theme.colors.red;
-        }
+  //   var count = 1;
+  //   ctx.beginPath();
 
-        count++;
-      }
-    }
-  };
+  //   for (var x = 0; x <= w; x += step) {
+  //     ctx.moveTo(x, 0);
+  //     ctx.lineTo(x, h);
+  //   }
+  //   // set the color of the line
+  //   ctx.strokeStyle = theme.colors.red;
+  //   ctx.lineWidth = 1;
+
+  //   // the stroke will actually paint the current path
+  //   ctx.stroke();
+
+  //   // for the sake of the example 2nd path
+  //   ctx.beginPath();
+  //   for (var y = 0; y <= h; y += step) {
+  //     ctx.moveTo(0, y);
+  //     ctx.lineTo(w, y);
+  //   }
+  //   // // set the color of the line
+  //   ctx.strokeStyle = 'rgb(20,20,20)';
+  //   // // just for fun
+  //   ctx.lineWidth = 1;
+  //   // for your original question - you need to stroke only once
+  //   ctx.stroke();
+
+  //   for (var y1 = 40; y1 <= h; y1 += step) {
+  //     for (var x1 = 0; x1 <= w - 40; x1 += step) {
+  //       // ctx.fillText('' + count, x1, y1);
+
+  //       if (_grids.includes(count)) {
+  //         ctx.fillStyle = theme.colors.red;
+  //         ctx.globalAlpha = 0.5;
+  //         ctx.fillRect(x1, y1 - step, step, step);
+  //         ctx.globalAlpha = 1;
+  //         ctx.fillStyle = theme.colors.red;
+  //       }
+
+  //       count++;
+  //     }
+  //   }
+  // };
 
   return (
-    <Flex direction="column" h="100%" w="100%" px="2rem" my="5rem">
-      <Heading variant="headingOne" mb="2rem">
+    <Flex
+      direction="column"
+      alignItems="center"
+      h="100%"
+      w="100%"
+      px="2rem"
+      py="2rem"
+      bg="blackLight"
+    >
+      <Heading variant="headingOne" mb="2rem" mt="2rem">
         Map Minter
       </Heading>
-      <Flex direction="row" mb="2rem">
-        <StyledInput
-          placeholder="Enter token ID"
-          onChange={(e) => setTokenIdInput(e.target.value)}
-        ></StyledInput>
-        <Button
-          isLoading={loading}
-          variant="primary"
-          onClick={() => connect()}
-          disabled={
-            tokenIdInput &&
-            parseInt(tokenIdInput) <= 9750 &&
-            !grids.includes(parseInt(tokenIdInput))
-              ? false
-              : true
-          }
-        >
-          Claim
-        </Button>
-      </Flex>
-      <Tabs variant="line" isFitted>
-        <TabList fontFamily="rubik" fontWeight="bold">
-          <Tab bg="black" color="red" _selected={{ bg: 'red', color: 'black' }}>
-            World Map
-          </Tab>
-          <Tab bg="black" color="red" _selected={{ bg: 'red', color: 'black' }}>
-            Claimed Maps
-          </Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <Text variant="textOne" mt="1rem" mb="2rem" maxW="100%">
-              Below is a 100x100 grid with each of them representing a map where
-              the monsters hide and lurk in the dark. The red highlighted
-              squares in the grid represent a map token that has been explored
-              by a hunter while the dark squares are unexplored and who knows
-              what hides in there.
-            </Text>
-            <canvas
-              id="grid"
-              style={{
-                background: theme.colors.blackLight,
-                width: '100%'
-              }}
-            ></canvas>
-          </TabPanel>
-          <TabPanel>
-            <Text variant="textOne" mt="1rem" mb="1rem" maxW="100%">
-              Find below the map token IDs that are explored in the above world
-              map. Click to re-explore a map. Or enter a token ID that is not
-              explored to mint it.
-            </Text>
 
-            {canvasDrawn && (
-              <SimpleGrid
-                columns={15}
-                gap={1}
-                style={{ backdropFilter: 'blur(.5rem)' }}
-              >
-                {grids.map((tokenId) => (
-                  <Box
-                    key={tokenId}
-                    bg="blackLight"
-                    color="red"
-                    p={3}
-                    fontFamily="jetbrains"
-                    cursor="pointer"
-                    _hover={{ bg: 'black' }}
-                    onClick={() => history.push(`/map/${tokenId}`)}
-                  >
-                    #{tokenId}
-                  </Box>
-                ))}
-              </SimpleGrid>
-            )}
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
+      <Text variant="textOne" mb="2rem">
+        Below is a grid of 10000 tiles with each of them representing a map
+        where the monsters hide and lurk in the dark. The red highlighted
+        squares in the grid represent a map token that has been explored by a
+        hunter while the dark squares are unexplored and who knows what hides in
+        there.
+      </Text>
+
+      <Text variant="textOne" mb="2rem">
+        Click on a explored tile to view more. Or click on a unexplored tile to
+        mint it.
+      </Text>
+
+      {grids.length === 0 && <Spinner size="xl" />}
+      {grids.length !== 0 && (
+        <MapTiles grids={grids} connect={connect} mint={mint} />
+      )}
 
       <Modal isOpen={modal} onClose={() => setModal(false)} isCentered>
         <ModalOverlay>
@@ -293,7 +231,7 @@ const World = () => {
             {context.chainID === 1 || context.chainID === '0x1' ? (
               <>
                 <Text variant="textOne" mb="2rem" fontSize="lg">
-                  Are you sure to mint token #{tokenIdInput}?
+                  Are you sure to mint token #{mintTokenId}?
                 </Text>
 
                 <Button
